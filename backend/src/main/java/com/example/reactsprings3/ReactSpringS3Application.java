eae994651ae0c7de6b089e89e8899fc2cec1f31a
dhslrl321 <dhslrl321@gmail.com>
2021-10-04 15:35:58 +0900

    commit

---
+++ backend/src/main/java/com/example/reactsprings3/ReactSpringS3Application.java
@@ -0,0 +1,13 @@
+package com.example.reactsprings3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringS3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactSpringS3Application.class, args);
+    }
+
+}
