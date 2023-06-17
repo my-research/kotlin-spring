28c1741ebaf5db0fcfa3a40ca67ed53a5643614b
dhslrl321 <dhslrl321@gmail.com>
2023-06-17 16:35:11 +0900

    commit

---
+++ src/main/kotlin/com/github/dhslrl321/kotlinspring/KotlinSpringApplication.kt
@@ -0,0 +1,11 @@
+package com.github.dhslrl321.kotlinspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/dhslrl321/kotlinspring/KotlinSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.dhslrl321.kotlinspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
